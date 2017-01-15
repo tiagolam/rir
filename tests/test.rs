60b60c1f6a212382e0d40c562e44590ffaea75a9
Tiago Lam <tiagolam@gmail.com>
2017-01-15 22:47:21 +0000

    Add "send only" test.
    In order to test if sending RTP packets is sucessful (mostly by sending
    to a receiving oRTP instance), a new test was added, named test_sending.
    
    It first initialises an RtpSession, then opens a local file and, in a
    while loop, while there's something to read, sends the payload read into
    the opened RtpSession.
    
    The test_connect was now renamed to the more appropriate name
    test_receiving

--- tests/test.rs
+++ tests/test.rs
@@ -6,11 +6,75 @@ extern crate log4rs;
 use std::thread;
 use std::str::FromStr;
 use std::net::{UdpSocket, SocketAddr};
+use std::io::prelude::*;
+use std::fs::File;
 
 use rir::rtp::{RtpSession, RtpPkt, RtpHeader};
 
 #[test]
-fn test_connect() {
+fn test_sending() {
+
+    let mut f = File::open("./tests/hello_world.txt").unwrap();
+
+    // Set up the local socket
+    let local_addr =  FromStr::from_str("127.0.0.1").unwrap();
+    let bind_socket = SocketAddr::new(local_addr, 0);
+    let conn = UdpSocket::bind(bind_socket);
+
+    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    info!("Running the send only test...");
+
+    let mut rtp_stream = RtpSession::connect_to(conn.unwrap(), SocketAddr::new(local_addr, 32000));
+
+    let v = vec![];
+
+    let mut rtp_pkt = &mut RtpPkt {
+        header: RtpHeader {
+            version: 2,
+            padding: 0,
+            ext: 0,
+            cc: 0,
+            marker: 0,
+            payload_type: 0,
+            seq_number: 0,
+            timestamp: 0,
+            ssrc: 0123456789,
+            csrc: v,
+        },
+        payload: vec![],
+    };
+
+    let mut buffer: [u8; 1400] = [0; 1400];
+    let mut seq = 0;
+    let mut ts = 0;
+
+    while let Ok(x) = f.read(&mut buffer) {
+        if x == 0 {
+            break;
+        }
+
+        rtp_pkt.header.seq_number = seq;
+        rtp_pkt.header.timestamp = ts;
+
+        debug!("Writing packet with payload {} and seq {}", x, seq);
+
+        rtp_pkt.payload = vec![0; x];
+        rtp_pkt.payload.clone_from_slice(&buffer[..x]);
+
+        rtp_stream.write(rtp_pkt);
+        thread::sleep_ms(160);
+
+        ts += 160;
+        if seq >= 65535 {
+            seq = 0;
+        } else {
+            seq += 160;
+        }
+    }
+}
+
+#[test]
+fn test_receiving() {
 
     // Set up the local socket
     let local_addr =  FromStr::from_str("127.0.0.1").unwrap();
@@ -38,9 +102,8 @@ fn test_connect() {
         payload: vec![],
     };
 
-    while true {
+    loop {
         rtp_stream.read(rtp_pkt);
-        //thread::sleep_ms(50);
     }
 }
 
